5c0c24e4fecfff88efffbc0e129e1a635c419ee0
andemosa <osayerieanderson@yahoo.com>
2023-10-31 01:06:08 +0100

    updated seo images

--- next-seo.config.js
+++ next-seo.config.js
@@ -9,19 +9,19 @@ export default {
     description: "Software Developer and Technical Writer",
     images: [
       {
-        url: "https://andemosa.vercel.app/site-preview.webp",
+        url: "https://andemosa.vercel.app/images/site-preview.webp",
         width: 1200,
         height: 675,
         alt: "Software Developer and Technical Writer",
       },
       {
-        url: "https://andemosa.vercel.app/og-image-800.webp",
+        url: "https://andemosa.vercel.app/images/og-image-800.webp",
         width: 800,
         height: 420,
         alt: "Software Developer and Technical Writer",
       },
       {
-        url: "https://andemosa.vercel.app/og-image-640.webp",
+        url: "https://andemosa.vercel.app/images/og-image-640.webp",
         width: 640,
         height: 336,
         alt: "Software Developer and Technical Writer",

84cfd515f4da28751ce6d9a6284fa9ee082cd168
andemosa <osayerieanderson@yahoo.com>
2024-09-16 14:47:38 +0100

    changed seo to new domain

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,27 +1,27 @@
 export default {
   title: "Anderson Osayerie | Software Developer",
   description: "Software Developer and Technical Writer",
-  canonical: "https://andemosa.vercel.app/",
+  canonical: "https://andemosa.tech/",
   openGraph: {
-    url: "https://andemosa.vercel.app/",
+    url: "https://andemosa.tech/",
     type: "website",
     title: "Anderson Osayerie | Software Developer",
     description: "Software Developer and Technical Writer",
     images: [
       {
-        url: "https://andemosa.vercel.app/images/site-preview.webp",
+        url: "https://andemosa.tech/images/site-preview.webp",
         width: 1200,
         height: 675,
         alt: "Software Developer and Technical Writer",
       },
       {
-        url: "https://andemosa.vercel.app/images/og-image-800.webp",
+        url: "https://andemosa.tech/images/og-image-800.webp",
         width: 800,
         height: 420,
         alt: "Software Developer and Technical Writer",
       },
       {
-        url: "https://andemosa.vercel.app/images/og-image-640.webp",
+        url: "https://andemosa.tech/images/og-image-640.webp",
         width: 640,
         height: 336,
         alt: "Software Developer and Technical Writer",

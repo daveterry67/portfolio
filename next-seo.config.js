c58d8716fa2049b40b05cb637731d50ad9f32016
Dappa David <daviddappa048@gmail.com>
2024-12-23 08:45:42 +0100

    chore: adjust seo file

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,37 +1,17 @@
 export default {
-  title: "Anderson Osayerie | Software Developer",
-  description: "Software Developer and Technical Writer",
-  canonical: "https://andemosa.tech/",
+  title: "Dappa David | Software Engineer",
+  description: "Software Engineer",
+  canonical: "https://portfoliodappa.vercel.app/",
   openGraph: {
-    url: "https://andemosa.tech/",
+    url: "https://portfoliodappa.vercel.app/",
     type: "website",
-    title: "Anderson Osayerie | Software Developer",
-    description: "Software Developer and Technical Writer",
-    images: [
-      {
-        url: "https://andemosa.tech/images/site-preview.webp",
-        width: 1200,
-        height: 675,
-        alt: "Software Developer and Technical Writer",
-      },
-      {
-        url: "https://andemosa.tech/images/og-image-800.webp",
-        width: 800,
-        height: 420,
-        alt: "Software Developer and Technical Writer",
-      },
-      {
-        url: "https://andemosa.tech/images/og-image-640.webp",
-        width: 640,
-        height: 336,
-        alt: "Software Developer and Technical Writer",
-      },
-    ],
-    site_name: "Anderson Osayerie | Software Developer",
+    title: "Dappa David | Software Developer (Web/Mobile)",
+    description: "Software Engineer",
+    site_name: "Dappa David | Software Engineer",
   },
   twitter: {
-    handle: "@andemosa",
-    site: "@andemosa",
+    handle: "@daveterry76",
+    site: "@daveterry76",
     cardType: "summary_large_image",
   },
 };
